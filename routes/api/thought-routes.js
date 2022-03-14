56848f9b47f42f1447c0c230bbd93f44d2203c62
btijerino16 <92263027+btijerino16@users.noreply.github.com>
2022-03-13 20:08:34 -0700

    adding api routes for thought and user

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+
+const {
+  getAllThoughts,
+  getThoughtById,
+  newThought,
+  updateThought,
+  removeThought,
+  createReaction,
+  removeReaction
+} = require('../../controllers/thought-controller');
+const { remove } = require('../../models/User');
+
+router
+  .route('/')
+  .get(getAllThoughts)
+  .post(newThought);
+
+router
+  .route('/:id')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(removeThought);
+
+router
+  .route('/:thoughtId/reactions')
+  .post(createReaction)
+  .delete(removeReaction)
+
+
+module.exports = router;
